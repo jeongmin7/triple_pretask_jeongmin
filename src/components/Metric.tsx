c1af1c3ce6a64ed671f60efc412871930be3cc40
jeongmin <lee.jeongmin1625@gmail.com>
2022-07-05 18:47:34 +0900

    feature: CountUp 애니메이션 구현

--- src/components/Metric.tsx
+++ src/components/Metric.tsx
@@ -1,23 +1,27 @@
 import React from "react";
+import useCountUp from "../hooks/useCountUp";
 import { MetricContainer, MetricItem, Strong } from "../styles/styles";
 const Metric = () => {
+  const userCount = useCountUp(700);
+  const reviewCount = useCountUp(100);
+  const planCount = useCountUp(470);
   return (
     <MetricContainer>
       <MetricItem>
         <Strong>
-          <span>700</span>만 명
+          <span>{userCount}</span>만 명
         </Strong>
         의 여행자 <br />
       </MetricItem>
       <MetricItem>
         <Strong>
-          <span>100</span>만 개
+          <span>{reviewCount}</span>만 개
         </Strong>
         의 여행 리뷰 <br />
       </MetricItem>
       <MetricItem>
         <Strong>
-          <span>470</span>만 개
+          <span>{planCount}</span>만 개
         </Strong>
         의 여행 일정 <br />
       </MetricItem>
